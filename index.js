a08c8ee67a74ab5ce7989bc4c41358a58becaed5
johnlloyd2002 <llanillojohnlloyd@gmail.com>
2024-06-08 15:03:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const btnLike1 = document.getElementById("btnLike1")
 const countLike1 = document.getElementById("countLike1")
-
 const btnLike2 = document.getElementById("btnLike2")
 const countLike2 = document.getElementById("countLike2")
 
