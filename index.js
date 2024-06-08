8a31cbda1123b1de85d4a3cd2ab8d4c7c731334f
johnlloyd2002 <llanillojohnlloyd@gmail.com>
2024-06-08 16:24:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 function submitComment(){
   commentbox.textContent += comment.value.toString() + "\n"
-  comment. value=""
+  comment.value=""
 }
 
 
